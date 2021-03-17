c58e5d1aeeb1ad08c2bb1f18b171e12ea3a563d7
Sergio G. Cruz Espinoza <seguce92@gmail.com>
2021-03-17 18:58:03 -0400

    @update classNames[Product.js]

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,7 @@ class Product extends React.Component {
 
         return (
             <div className="bg-white rounded-md shadow-lg flex flex-col">
-                <h2 className="text-center mt-3">{ mark }</h2>
+                <h2 className="text-center mt-3 text-gray-600">{ mark }</h2>
                 { 
                     <img src={ 
                         mark === 'Apple' ? 
@@ -22,7 +22,7 @@ class Product extends React.Component {
                         } className="w-1/2 mx-auto relative my-4" />
                 }
                 
-                <h2 className="text-center mb-3">{ phone.modelo }</h2>
+                <h2 className="text-center mb-3 text-xs uppercase text-gray-600 font-bold">{ phone.modelo }</h2>
 
                 <div className="border-t px-4 pt-6 pb-4 relative">
                     <button
@@ -31,7 +31,7 @@ class Product extends React.Component {
                         <svg className="w-6 h-6 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                     </button>
                     <div className="flex justify-between items-center">
-                        <h4 class="text-lg">$ { phone.precio }</h4> <span>Color: { phone.color }</span>
+                        <h4 className="text-lg text-gray-600">$ { phone.precio }</h4> <span className="text-gray-600">Color: { phone.color }</span>
                     </div>
                     <div className="flex"></div>
                 </div>
